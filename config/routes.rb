f23d1c6bc59c94544b064e30fa6ac4a474efb04c
Vasilii Bulkovskii <vasilii.bulkovskii@jetruby.com>
2023-06-15 12:25:25 +0500

    add validation to user

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root "dashboard#index" 
-  devise_for :users
-
   mount Sidekiq::Web => "/sidekiq"
+  devise_for :users
+  root "dashboard#index" 
+ 
+  post "users/restore", to: "users#restore_balance"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_173332) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_064929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_173332) do
     t.datetime "remember_created_at"
     t.string "first_name"
     t.string "last_name"
-    t.integer "balance", default: 0, null: false
+    t.decimal "balance", default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
